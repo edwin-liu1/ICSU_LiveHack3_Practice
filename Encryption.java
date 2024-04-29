aa628c64cff433cbdcd7c2180d8708f2fb074390
Edwin_liu1 <Edwin.liu25@ycdsbk12.ca>
2024-04-29 11:19:34 -0400

    doing the repository shuffle

--- Encryption.java
+++ Encryption.java
@@ -6,6 +6,25 @@ class Encryption extends ConsoleProgram {
   */
   
   public void run() {
+    System.out.println(encryptMessage("ogabooga", -24) );
+  }
 
+  /*
+   * a basic caesar cipher done poorly in java.
+   * 
+   * @param message. The massage you want encrypted.
+   * @param shift. The number of spaces the characters is shifted by.
+   * 
+   * @return encrypt. The finnal encrypted message.
+   */
+  public String encryptMessage(String message, int shift){
+    char letter;
+    String encrypt = "";
+    for (int i = 0; i < message.length(); i++){
+      letter = message.charAt(i);
+      letter += shift;
+      encrypt += letter;
+    }
+    return encrypt;
   }
 }
